af210cebbe40b770e05ad7f6420cffa46b5c5175
AndresCastillo28 <ansa2000@hotmail.es>
2024-01-25 10:51:22 -0500

    get all contact information

--- src/main/java/com/andrescastillo/portafolio/rest/ContactInformationRest.java
+++ src/main/java/com/andrescastillo/portafolio/rest/ContactInformationRest.java
@@ -4,13 +4,19 @@ import com.andrescastillo.portafolio.entity.ContactInformation;
 import com.andrescastillo.portafolio.utils.ApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping(path = "/contact-information")
 public interface ContactInformationRest {
 
     @PostMapping
     ResponseEntity<ApiResponse<ContactInformation>> save(@Valid @RequestBody ContactInformation contactInformation);
+
+    @GetMapping
+    ResponseEntity<ApiResponse<List<ContactInformation>>> findAll();
 }
--- src/main/java/com/andrescastillo/portafolio/service/ContactInformationService.java
+++ src/main/java/com/andrescastillo/portafolio/service/ContactInformationService.java
@@ -2,6 +2,10 @@ package com.andrescastillo.portafolio.service;
 
 import com.andrescastillo.portafolio.entity.ContactInformation;
 
+import java.util.List;
+
 public interface ContactInformationService {
     ContactInformation save(ContactInformation contactInformation);
+
+    List<ContactInformation> findAll();
 }
